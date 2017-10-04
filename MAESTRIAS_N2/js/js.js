94cd0d74629f1159d5a9648222daebb91b2f8049
Maxwell Kenned <maxwell.kenned@softcomtec.info>
2017-10-04 13:02:40 -0300

    SISTEMA DE MAESTRIAS 1.3.0

--- MAESTRIAS_N2/js/js.js
+++ MAESTRIAS_N2/js/js.js
@@ -1,79 +1,9 @@
-var m = new Maestria();
-
-function lista(query = {}){
-  var html = '';
-  $("#tableFaq tr, td").remove();
-  if(!$.isEmptyObject(query)){
-    $.each(query, function(i, d){
-      var value = d.value;
-      var grupo = d.grupo;
-      $.each(ARRAYFAQ, function(i, c) {
-        var cor = setCor(c.nivel);
-        if(grupo == "faq"){
-          if((c[grupo].toString()).indexOf(value.toString()) != -1){
-            html += "<tr style='background-color:"+cor+"'>";
-            html += "<td id='nivel' >"+c.nivel+"</td>";
-            html += "<td id='grupo'>"+c.grupo+"</td>";
-            html += "<td id='faq'>"+c.faq+"</td>";
-            html += "<td id='desc'>"+c.desc+"</td>";
-            html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
-            html += "</tr>";
-          }
-        }else{
-          if((c[grupo]).toLowerCase().indexOf(value.toLowerCase()) != -1){
-            html += "<tr style='background-color:"+cor+"'>";
-            html += "<td id='nivel'>"+c.nivel+"</td>";
-            html += "<td id='grupo'>"+c.grupo+"</td>";
-            html += "<td id='faq'>"+c.faq+"</td>";
-            html += "<td id='desc'>"+c.desc+"</td>";
-            html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
-            html += "</tr>";
-          }
-        }
-      });
-    });
-    $('#tableFaq').append(html);
-  } else{
-    $.each(ARRAYFAQ, function(i, c) {
-      var cor = setCor(c.nivel);
-      html += "<tr style='background-color:"+cor+"'>";
-      html += "<td id='nivel'>"+c.nivel+"</td>";
-      html += "<td id='grupo'>"+c.grupo+"</td>";
-      html += "<td id='faq'>"+c.faq+"</td>";
-      html += "<td id='desc'>"+c.desc+"</td>";
-      html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
-      html += "</tr>";
-    });
-    $('#tableFaq').append(html);
-  }
-};
-lista();
-function AtListaGrupo(grupo){
+var m = ARRAYFAQ;
+lista(m);
+function AtListaGrupo(grupo=""){
   if(!$.isEmptyObject(grupo.value)){
-    lista({"grupo": grupo});
+    lista(m, {"grupo": grupo});
   }else{
-    lista();
+    lista(m);
   }
-
 };
-function setCor(c){
-  var cor;
-  switch(c){
-    case 1:
-      cor = "#d5ffe0";
-      break;
-    case 2:
-      cor = "#d5e2ff";
-      break;
-    case 3:
-      cor = "#f4ce42";
-      break;
-    case 4:
-      cor = "#c7dba7";
-      break;
-    default:
-      cor = '';
-    break;
-  }
-  return cor;
-}
---
+++ js/js.js
@@ -0,0 +1,72 @@
+function lista(bd, query = {}){
+  var html = '';
+  var ARRAYFAQ = bd;
+  var colorBW = $("#colorBW");
+  $("#tableFaq tr, td").remove();
+  if(!$.isEmptyObject(query)){
+    $.each(query, function(i, d){
+      var value = d.value;
+      var grupo = d.grupo;
+
+      $.each(ARRAYFAQ, function(i, c) {
+        var cor = colorBW[0].checked?'':setCor(c.nivel);
+        if(grupo == "faq"){
+          if((c[grupo].toString()).indexOf(value.toString()) != -1){
+            html += "<tr style='background-color:"+cor+"'>";
+            html += "<td id='nivel' >"+c.nivel+"</td>";
+            html += "<td id='grupo'>"+c.grupo+"</td>";
+            html += "<td id='faq'>"+c.faq+"</td>";
+            html += "<td id='desc'>"+c.desc+"</td>";
+            html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
+            html += "</tr>";
+          }
+        }else{
+          if((c[grupo]).toLowerCase().indexOf(value.toLowerCase()) != -1){
+            html += "<tr style='background-color:"+cor+"'>";
+            html += "<td id='nivel'>"+c.nivel+"</td>";
+            html += "<td id='grupo'>"+c.grupo+"</td>";
+            html += "<td id='faq'>"+c.faq+"</td>";
+            html += "<td id='desc'>"+c.desc+"</td>";
+            html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
+            html += "</tr>";
+          }
+        }
+      });
+    });
+    $('#tableFaq').append(html);
+  } else{
+    $.each(ARRAYFAQ, function(i, c) {
+      var cor = colorBW[0].checked?'':setCor(c.nivel);
+      html += "<tr style='background-color:"+cor+"'>";
+      html += "<td id='nivel'>"+c.nivel+"</td>";
+      html += "<td id='grupo'>"+c.grupo+"</td>";
+      html += "<td id='faq'>"+c.faq+"</td>";
+      html += "<td id='desc'>"+c.desc+"</td>";
+      html += "<td id='tmfaq'>"+c.tmfaq+"</td>";
+      html += "</tr>";
+    });
+    $('#tableFaq').append(html);
+  }
+};
+
+function setCor(c){
+  var cor;
+  switch(c){
+    case 1:
+      cor = "#d5ffe0";
+      break;
+    case 2:
+      cor = "#d5e2ff";
+      break;
+    case 3:
+      cor = "#f4ce42";
+      break;
+    case 4:
+      cor = "#c7dba7";
+      break;
+    default:
+      cor = '';
+    break;
+  }
+  return cor;
+}
