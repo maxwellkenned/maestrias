c65e7c3023b862ec657e2b3cff6688288c732f6b
Maxwell Kenned <maxwell.kenned@softcomtec.info>
2017-10-04 13:14:33 -0300

    SISTEMA DE MAESTRIAS 1.3.4

--- MAESTRIAS_SPED/js/js.js
+++ MAESTRIAS_SPED/js/js.js
@@ -1,16 +1,9 @@
-var m;
-$.getScript( "./js/bd.js" ).done(function( script, textStatus ) {
-    console.log( textStatus );
-    console.log(script);
-    m = ARRAYFAQ;
-  });
+var m = ARRAYFAQ;
+lista(m);
 function AtListaGrupo(grupo=""){
   if(!$.isEmptyObject(grupo.value)){
     lista(m, {"grupo": grupo});
   }else{
     lista(m);
   }
-};
-window.onload = function() {
-  lista(m);
 };
