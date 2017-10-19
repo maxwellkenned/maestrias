aa245771d7b0971a1066a168d29dd151024d9cf9
Maxwell Kenned <maxwell.kenned@softcomtec.info>
2017-10-19 13:10:45 -0300

    SISTEMA DE MAESTRIAS 1.8.5

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light bg-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <a class="navbar-brand" href="/">FAQs e Maestrias</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -6,16 +6,16 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="./FAQ_GERAL/faq.php">FAQs Gerais <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="http://maestrias.herokuapp.com/FAQ_GERAL/faq.php">FAQs Gerais <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Maestrias
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="./MAESTRIAS_N1/n1.php">Maestrias N1</a>
-            <a class="dropdown-item" href="./MAESTRIAS_SPED/sped.php">Maestrias SPED</a>
-            <a class="dropdown-item" href="./MAESTRIAS_N2/n2.php">Maestrias N2</a>
+            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N1/n1.php">Maestrias N1</a>
+            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_SPED/sped.php">Maestrias SPED</a>
+            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N2/n2.php">Maestrias N2</a>
           </div>
           </li>
           <li class="nav-item dropdown">
