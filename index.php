d05b836d64be679d2cff7fff45237aa300b21ac5
Maxwell Kenned <kenned123@gmail.com>
2017-12-07 15:59:12 +0000

    Adicionado o Guida Rápidp RFC N2

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php include_once("navbar.php"); ?>
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>MAESTRIAS SOFTCOM</title>
+    <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="./css/style.css" />
+    <link rel="icon" href="img/icone.png"  type="image/png">
+  </head>
+  <body>
+      
+    <?php include_once("navbar.php"); ?>
+    <?php include_once("home.html"); ?>
+    
+    <script type="text/javascript" src="./js/jquery.js"></script>
+    <script type="text/javascript" src="./js/popper.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+  </body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,37 +1,40 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a class="navbar-brand" href="/">FAQs e Maestrias</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="http://maestrias.herokuapp.com/FAQ_GERAL/faq.php">FAQs Gerais <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Maestrias
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N1/n1.php">Maestrias N1</a>
-            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_SPED/sped.php">Maestrias SPED</a>
-            <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N2/n2.php">Maestrias N2</a>
-          </div>
-          </li>
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Mapa Mental
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/MapaMental_SistemaLento2.png">Sistema Lento</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Envio%20de%20E-mail5.png">Envio de Email</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Etiquetas2.png">Etiquetas</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/MapaMental-Boleto.png">Boleto</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Estoque_3.png">Estoque</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Instala%C3%A7%C3%A3oMaquinaFormatada.png">Instalação</a>
-            <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/RFC%20N2.png">RFC N2</a>
-          </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="/">FAQs e Maestrias</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="http://maestrias.herokuapp.com/FAQ_GERAL/faq.php">FAQs Gerais <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Maestrias
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N1/n1.php">Maestrias N1</a>
+              <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_SPED/sped.php">Maestrias SPED</a>
+              <a class="dropdown-item" href="http://maestrias.herokuapp.com/MAESTRIAS_N2/n2.php">Maestrias N2</a>
+            </div>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Mapa Mental
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/MapaMental_SistemaLento2.png">Sistema Lento</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Envio%20de%20E-mail5.png">Envio de Email</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Etiquetas2.png">Etiquetas</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/MapaMental-Boleto.png">Boleto</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Estoque_3.png">Estoque</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/Instala%C3%A7%C3%A3oMaquinaFormatada.png">Instalação</a>
+              <a class="dropdown-item" target="_blank" href="http://177.43.232.3:10000/helptools2/public/upload/images/promover/RFC%20N2.png">RFC N2</a>
+            </div>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" target="_blank" href="/arquivos/Guia_Rapido_RFC_N2.pdf">Guia Rápido RFC N2</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
